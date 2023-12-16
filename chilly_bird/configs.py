787a04d075bd45f310d737913831b598e61ecd59
relicks <>
2023-12-16 10:31:34 +0100

    still working spaghetti

--- chilly_bird/configs.py
+++ chilly_bird/configs.py
@@ -5,6 +5,14 @@ from omegaconf import MISSING, OmegaConf
 
 
 @dataclass
+class AssetsConf:
+    _path: Path = MISSING
+    fonts_path: Path = MISSING
+    img_path: Path = MISSING
+    sound_path: Path = MISSING
+
+
+@dataclass(frozen=True)
 class Window:
     fps: int = 60
     screen_width: int = 432
@@ -26,6 +34,7 @@ class Fonts:
 class MainSceneAssets:
     bg_img: Path = MISSING
     road_texture: Path = MISSING
+    pipe_img: Path = MISSING
     restart_button_img: Path = MISSING
     disappointed_girl_img: Path = MISSING
     bg_music: Path = MISSING
@@ -33,11 +42,24 @@ class MainSceneAssets:
 
 @dataclass
 class MainConfig:
+    assets: AssetsConf = field(default_factory=lambda: AssetsConf())
     window: Window = field(default_factory=lambda: Window())
     fonts: Fonts = field(default_factory=lambda: Fonts())
     main_scene: MainSceneAssets = field(default_factory=lambda: MainSceneAssets())
 
 
+def load_config(path: str = "./conf/config.yaml") -> MainConfig:
+    conf_s: MainConfig = OmegaConf.structured(MainConfig)
+    conf_f = OmegaConf.load("./conf/config.yaml")
+    conf: MainConfig = OmegaConf.merge(conf_s, conf_f)  # type: ignore
+    return conf
+
+
 if __name__ == "__main__":
-    conf: MainConfig = OmegaConf.structured(MainConfig)
+    conf_s: MainConfig = OmegaConf.structured(MainConfig)
+    conf_f = OmegaConf.load("./conf/config.yaml")
+    conf: MainConfig = OmegaConf.merge(conf_s, conf_f)  # type: ignore
+    # print(OmegaConf.to_yaml(conf_s))
+    # print(OmegaConf.to_yaml(conf_f))
     print(OmegaConf.to_yaml(conf))
+    print(conf.assets._path.resolve())
