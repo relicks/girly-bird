ee0ca767059d72fee3c75b12d61153f9958bdfde
relicks <>
2023-12-17 22:26:56 +0200

    move it

--- src/chilly_bird/objects.py
+++ src/chilly_bird/objects.py
@@ -79,6 +79,11 @@ class Pipe(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.pipe_gap = pipe_gap
         self.scroll_speed = scroll_speed
+
+        self.counter = 0
+        self.div = 10
+        self.step = 0.501
+
         if location == 1:  # Pipe pointing up
             # ? +35 pixels in order to create a gap
             self.rect.topleft = (x, round(y + self.pipe_gap / 2))
@@ -92,6 +97,15 @@ class Pipe(pg.sprite.Sprite):
     def update(self):
         logger.trace("Pipe updating")
         self.rect.x -= self.scroll_speed  # Forces pipes to constantly move to the left
+
+        if self.counter >= 0:
+            self.rect.y -= self.step
+            if self.counter >= self.div:
+                self.counter -= 2 * self.div
+        else:
+            self.rect.y += self.step
+        self.counter += self.step
+
         if self.rect.right < 0:
             logger.debug("Pipe moved out of screen, killing it")
             # As soon as the rightmost point of a pipe disappears
