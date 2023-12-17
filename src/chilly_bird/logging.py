66faf7b412e94072cca6331642060e6893711d68
relicks <>
2023-12-17 23:22:26 +0100

    tidying up

--- src/chilly_bird/logging.py
+++ src/chilly_bird/logging.py
@@ -8,15 +8,15 @@ import loguru
 
 def configure_logger(
     logger_: loguru.Logger,
-    logger_level: str = "TRACE",
-    log_file_path: str = "./logs/",
+    level: str = "TRACE",
+    logs_path: str = "./logs/",
     print_stdout: bool = False,
 ) -> None:
-    path = Path(log_file_path).resolve() / "runtime_{time}.log"
+    path = Path(logs_path).resolve() / "runtime_{time}.log"
     path.parent.mkdir(parents=True, exist_ok=True)
 
     logger_.remove(0)
-    logger_.add(path, level=logger_level, retention=5)
+    logger_.add(path, level=level, retention=5)
 
     if print_stdout:
         logger_.add(sys.stderr, level="INFO")
