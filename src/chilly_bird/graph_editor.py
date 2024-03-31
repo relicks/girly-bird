b07c7cc09dfd82b9424011279e69c1d67305b2d8
relicks <>
2024-03-31 16:54:46 +0200

    added missing docs

--- src/chilly_bird/graph_editor.py
+++ src/chilly_bird/graph_editor.py
@@ -1,24 +1,24 @@
+"""A user-facing bird sprite editor."""
+
 import pygame as pg
 from typing_extensions import Self
 
-from . import utils
-from .types import ColorValue, Coordinate, RectValue
-
-
-def ellipsify(image: pg.Surface) -> None:
-    rect = image.get_rect()
-    w, h = rect.w, rect.h
-    mask_surface = pg.Surface((w, h), pg.SRCALPHA)
-    pg.draw.ellipse(mask_surface, (255, 255, 255, 255), rect, 0)
-    ellipse_mask = pg.mask.from_surface(mask_surface)
-    for y in range(h):
-        for x in range(w):
-            if not ellipse_mask.get_at((x, y)):
-                image.set_at((x, y), (0, 0, 0, 0))
+from chilly_bird import utils
+from chilly_bird.types import ColorValue, Coordinate, RectValue
 
 
 class GraphEditor:
+    """GraphEditor class for editing bird sprites."""
+
     def __init__(self, max_screen_w: int = 800, max_screen_h: int = 600) -> None:
+        """Initialize the GraphEditor with screen dimensions.
+
+        Args:
+        ----
+            max_screen_w: editors screen's width
+            max_screen_h: editors screen's height
+
+        """
         self.FPS = 24
         self.circle_color = pg.color.Color("black")
         self.curr_color = pg.color.Color("orangered2")
@@ -36,6 +36,17 @@ class GraphEditor:
         self.background = self.create_checkered_background(10)
 
     def create_checkered_background(self, tile_size: int = 20) -> pg.Surface:
+        """Create a checkered background for the editor.
+
+        Args:
+        ----
+            tile_size: a size of a square tile, in px
+
+        Returns:
+        -------
+            Grey checkered background.
+
+        """
         color1 = pg.Color(128, 128, 128)  # Grey color  1
         color2 = pg.Color(64, 64, 64)  # Grey color  2
 
@@ -54,44 +65,66 @@ class GraphEditor:
         return checkered_background
 
     def start_selection(self, pos: Coordinate) -> None:
+        """Start a new selection at the given position."""
         self.curr_start = pos
 
     def new_selection(self, pos: Coordinate) -> None:
+        """Update the current selection based on the new position."""
         if pg.math.Vector2(self.curr_start).distance_to(pos) >= self.min_circle_size:
             self.selected_rect = utils.create_rect(self.curr_start, pos)
 
-    def draw_circle(self, color: ColorValue, rect: RectValue, width: int = 1) -> None:
+    def draw_ellipse(self, color: ColorValue, rect: RectValue, width: int = 1) -> None:
+        """Draw an hollow ellipse on the screen, bounded by the given `rect`.
+
+        Args:
+        ----
+            color: color of the ellipse's border
+            rect: ellipse's bounding box
+            width: width of the ellipse's border, in px
+
+        """
         pg.draw.ellipse(self.screen, color, rect, width=width)
 
     def draw_selection(self) -> None:
+        """Draw the current selection on the screen."""
         if self.selected_rect:
-            self.draw_circle(self.circle_color, self.selected_rect)
+            self.draw_ellipse(self.circle_color, self.selected_rect)
 
     def draw_current(self, pos: Coordinate) -> None:
+        """Draw the current selection shape, called when selection changes."""
         rect = utils.create_rect(self.curr_start, pos)
-        self.draw_circle(self.curr_color, rect)
+        self.draw_ellipse(self.curr_color, rect)
 
     def draw_user_image(self) -> None:
+        """Draw the user-selected image on the screen."""
         if self.user_image:
             self.screen.blit(self.user_image, (0, 0))
         else:
             self.screen.fill(pg.color.Color("white"))
 
     def draw_background(self) -> None:
-        # Blit the background onto the screen
+        """Blit the background onto the screen."""
         self.screen.blit(self.background, (0, 0))
 
     def get_image_selected(self) -> pg.Surface | None:
+        """Return the selected image as a pygame Surface."""
         if self.selected_rect and self.user_image:
             img = pg.Surface(
                 (self.selected_rect.w, self.selected_rect.h)
             ).convert_alpha()
             img.blit(self.user_image, (0, 0), area=self.selected_rect)
-            ellipsify(img)
+            self.ellipsify(img)
             return img
         return None
 
     def run(self) -> pg.Surface | None:
+        """Run the editor, allowing the user to select and edit the image.
+        
+        Returns
+        -------
+            User's selection as a pygame Surface or None, if no selection was made.
+
+        """
         if self.user_image is None:
             return None
         clock = pg.time.Clock()
@@ -123,7 +156,22 @@ class GraphEditor:
             pg.display.update()
 
     def __enter__(self) -> Self:
+        """Enter the context manager."""
         return self
 
     def __exit__(self, *exc_details) -> None:
+        """Exit the context manager, resetting the screen mode (size)."""
         pg.display.set_mode(self.previous_screen_mode)
+
+    @staticmethod
+    def ellipsify(image: pg.Surface) -> None:
+        """Convert the `image` to an ellipsoidal shape, in-place."""
+        rect = image.get_rect()
+        w, h = rect.w, rect.h
+        mask_surface = pg.Surface((w, h), pg.SRCALPHA)
+        pg.draw.ellipse(mask_surface, (255, 255, 255, 255), rect, 0)
+        ellipse_mask = pg.mask.from_surface(mask_surface)
+        for y in range(h):
+            for x in range(w):
+                if not ellipse_mask.get_at((x, y)):
+                    image.set_at((x, y), (0, 0, 0, 0))
